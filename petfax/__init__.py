9442e3cb15b4b10133e179a8af10dad10a502fcb
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-06-14 20:01:36 -0500

    finished blueprint activity

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,21 @@
-from flask import Flask 
+from flask import Flask
 
-def create_app(): 
+# factory
+def create_app():
     app = Flask(__name__)
 
+    # index route
     @app.route('/')
-    def hello(): 
+    def index(): 
         return 'Hello, PetFax!'
-    
-    #register pet blueprint
-    from . import pet
+
+    # register pet blueprint 
+    from . import pet 
     app.register_blueprint(pet.bp)
-    
-    return app
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
+    return app            
